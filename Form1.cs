bbe00731a2652ad140753a0e55dff7be7086bc28
Evgeniy Reshetnikov <beevasya@ya.ru>
2023-11-16 08:26:41 +0400

    task-1 реализовано

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClockApp.models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -12,11 +14,11 @@ namespace ClockApp
 {
     public partial class Form1 : Form
     {
-        private List<models.WorldTimeItem> items;
+        private ObservableCollection<models.WorldTimeItem> items;
         public Form1()
         {
             InitializeComponent();
-            items = new List<models.WorldTimeItem>();
+            items = new ObservableCollection<models.WorldTimeItem>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -25,26 +27,87 @@ namespace ClockApp
             comboBox1.DataSource = tz;
             comboBox1.DisplayMember = "DisplayName";
             comboBox1.ValueMember = "Id";
+            items.CollectionChanged += Items_CollectionChanged;
+            timer1.Start();
+        }
+
+        private void Items_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Add:
+                    AddTime((WorldTimeItem)e.NewItems[0]);
+                    break;
+                case System.Collections.Specialized.NotifyCollectionChangedAction.Remove:
+                    RemoveTime((WorldTimeItem)e.OldItems[0]);
+                    break;
+            }
+        }
+        private void AddTime(WorldTimeItem item)
+        {
+            string tzName = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(i => i.Id == item.Id).DisplayName;
+            GroupBox gb = new GroupBox();
+            gb.Height = 50;
+            gb.Width = 770;
+            Label name = new Label();
+            name.Text = tzName;
+            name.Location = new Point(5, 10);
+            name.Width = 700;
+            name.Height = 20;
+            Label time = new Label();
+            time.Text = "";
+            time.Location = new Point(5, 30);
+            time.Width = 700;
+            time.Height = 20;
+            time.Tag = gb;
+            Button btn = new Button();
+            btn.Text = "X";
+            btn.Location = new Point(740, 10);
+            btn.Width = 20;
+            btn.Height = 20;
+            btn.Click += Btn_Click;
+
+            gb.Controls.Add(name);
+            gb.Controls.Add(time);
+            gb.Controls.Add(btn);
+            btn.Tag = gb;
+            gb.Tag = (string)comboBox1.SelectedValue;
+            flowLayoutPanel1.Controls.Add(gb);
+            item.control = time;
+        }
+        private void RemoveTime(WorldTimeItem item)
+        {
+            var gb = (GroupBox)((Label)item.control).Tag;
+            gb.Dispose();
         }
 
         private void button1_Click(object sender, EventArgs ev)
         {
             if (comboBox1.SelectedIndex != -1)
             {
-                GroupBox gb = new GroupBox();
-                gb.Height = 50;
-                Label name = new Label();
-                name.Text = comboBox1.SelectedText;
-                name.Location = new Point(5, 5);
-                Label time = new Label();
-                time.Text = "Время";
-                time.Location = new Point(5, 20);                
+                items.Add(new WorldTimeItem() { Id = (string)comboBox1.SelectedValue });
+            }
+        }
 
-                gb.Controls.Add(name);
-                gb.Controls.Add(time);
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            var gb = (GroupBox)((Button)sender).Tag;
+            string id = (string)gb.Tag;
+            var item = items.FirstOrDefault(i => i.Id == id);
+            items.Remove(item);
+        }
 
-                flowLayoutPanel1.Controls.Add(gb);
-                items.Add(new models.WorldTimeItem { Id = (string)comboBox1.SelectedValue });
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (var item in items)
+            {
+                var tz = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(i => i.Id == item.Id);
+                item.control.Text = (DateTime.UtcNow + tz.BaseUtcOffset).ToString();
             }
         }
     }
