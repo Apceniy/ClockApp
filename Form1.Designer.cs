f2f680aa7d5ba63bfa9ebcf8edaaafe29ccb68ff
Apceniy <yandexars@gmail.com>
2023-11-22 16:56:33 +0400

    Частично реализована адаптивность дизайна

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,17 +32,17 @@ namespace ClockApp
             this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPageClocks = new System.Windows.Forms.TabPage();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanelTimezones = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBoxAddTimezone = new System.Windows.Forms.GroupBox();
+            this.buttonAddTimezone = new System.Windows.Forms.Button();
+            this.comboBoxAddTimezone = new System.Windows.Forms.ComboBox();
             this.tabPageAlarms = new System.Windows.Forms.TabPage();
             this.tabPageCountdowns = new System.Windows.Forms.TabPage();
             this.tabPageStopwatches = new System.Windows.Forms.TabPage();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tabControl1.SuspendLayout();
             this.tabPageClocks.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.groupBoxAddTimezone.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -63,8 +63,8 @@ namespace ClockApp
             // 
             // tabPageClocks
             // 
-            this.tabPageClocks.Controls.Add(this.flowLayoutPanel1);
-            this.tabPageClocks.Controls.Add(this.groupBox1);
+            this.tabPageClocks.Controls.Add(this.flowLayoutPanelTimezones);
+            this.tabPageClocks.Controls.Add(this.groupBoxAddTimezone);
             this.tabPageClocks.Location = new System.Drawing.Point(4, 22);
             this.tabPageClocks.Name = "tabPageClocks";
             this.tabPageClocks.Padding = new System.Windows.Forms.Padding(3);
@@ -73,44 +73,50 @@ namespace ClockApp
             this.tabPageClocks.Text = "Мировое время";
             this.tabPageClocks.UseVisualStyleBackColor = true;
             // 
-            // flowLayoutPanel1
+            // flowLayoutPanelTimezones
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 56);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(775, 360);
-            this.flowLayoutPanel1.TabIndex = 1;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.flowLayoutPanelTimezones.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Location = new System.Drawing.Point(3, 6);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(781, 44);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Добавление новой временной зоны";
+            this.flowLayoutPanelTimezones.Location = new System.Drawing.Point(9, 56);
+            this.flowLayoutPanelTimezones.Name = "flowLayoutPanelTimezones";
+            this.flowLayoutPanelTimezones.Size = new System.Drawing.Size(775, 360);
+            this.flowLayoutPanelTimezones.TabIndex = 1;
             // 
-            // button1
+            // groupBoxAddTimezone
             // 
-            this.button1.Location = new System.Drawing.Point(691, 16);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Добавить";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.groupBoxAddTimezone.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxAddTimezone.Controls.Add(this.buttonAddTimezone);
+            this.groupBoxAddTimezone.Controls.Add(this.comboBoxAddTimezone);
+            this.groupBoxAddTimezone.Location = new System.Drawing.Point(3, 6);
+            this.groupBoxAddTimezone.Name = "groupBoxAddTimezone";
+            this.groupBoxAddTimezone.Size = new System.Drawing.Size(781, 44);
+            this.groupBoxAddTimezone.TabIndex = 0;
+            this.groupBoxAddTimezone.TabStop = false;
+            this.groupBoxAddTimezone.Text = "Добавление новой временной зоны";
             // 
-            // comboBox1
+            // buttonAddTimezone
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(6, 17);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(679, 21);
-            this.comboBox1.TabIndex = 0;
+            this.buttonAddTimezone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAddTimezone.Location = new System.Drawing.Point(691, 16);
+            this.buttonAddTimezone.Name = "buttonAddTimezone";
+            this.buttonAddTimezone.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddTimezone.TabIndex = 1;
+            this.buttonAddTimezone.Text = "Добавить";
+            this.buttonAddTimezone.UseVisualStyleBackColor = true;
+            this.buttonAddTimezone.Click += new System.EventHandler(this.buttonAddTimezone_Click);
+            // 
+            // comboBoxAddTimezone
+            // 
+            this.comboBoxAddTimezone.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxAddTimezone.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAddTimezone.FormattingEnabled = true;
+            this.comboBoxAddTimezone.Location = new System.Drawing.Point(6, 17);
+            this.comboBoxAddTimezone.Name = "comboBoxAddTimezone";
+            this.comboBoxAddTimezone.Size = new System.Drawing.Size(679, 21);
+            this.comboBoxAddTimezone.TabIndex = 0;
             // 
             // tabPageAlarms
             // 
@@ -157,7 +163,7 @@ namespace ClockApp
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPageClocks.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
+            this.groupBoxAddTimezone.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -169,10 +175,10 @@ namespace ClockApp
         private System.Windows.Forms.TabPage tabPageAlarms;
         private System.Windows.Forms.TabPage tabPageCountdowns;
         private System.Windows.Forms.TabPage tabPageStopwatches;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBoxAddTimezone;
+        private System.Windows.Forms.Button buttonAddTimezone;
+        private System.Windows.Forms.ComboBox comboBoxAddTimezone;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelTimezones;
         private System.Windows.Forms.Timer timer1;
     }
 }
